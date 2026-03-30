afda6b2cbb68b3c510bfbf80f920e28b7541a906
Zaheer Babar <zaheerbaberkhan@gmail.com>
2026-03-30 10:50:12 -0700

    added playwright test and then added playwright workflow

---
+++ playwright.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from '@playwright/test';
+export default defineConfig({
+  webServer: {
+    command: 'npm run start',
+    url: 'http://localhost:8080',
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
+  use: {
+    baseURL: 'http://localhost:8080/',
+  },
+});
